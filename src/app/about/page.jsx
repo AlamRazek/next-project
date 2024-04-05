0a3266fcd050676db497e3334d3bef3d8cd49b63
AlamRazek <ohidul933@gmail.com>
2024-04-05 03:55:01 +0100

    about added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,10 +3,22 @@ import React from "react";
 
 const AboutPage = () => {
   return (
-    <div>
-      <div className="relative  w-[100%] h-96 ">
-        <Image src="/about.png" alt="" fill></Image>
+    <div className="">
+      <div className="">
+        <h2>About Agency</h2>
+        <h2>
+          We create digital ideas that are bigger, bolder,braver and better.
+        </h2>
+        <p>
+          We create digital ideas that are bigger,bolder nad braver.We believe
+          in good ideas flexibility and precission. Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Facilis, animi atque. Quasi impedit quia
+          deleniti eos minus laboriosam temporibus harum nesciunt, beatae et
+          sunt, natus nulla architecto eveniet excepturi fuga tenetur aliquam
+          deserunt error consequatur laborum autem unde aperiam. Reprehenderit.
+        </p>
       </div>
+      <div className=""></div>
     </div>
   );
 };
