3500dbca3601f4c611d44efb8d74ee4ed0505501
AlamRazek <ohidul933@gmail.com>
2024-04-05 03:59:06 +0100

    hero section added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const AboutPage = () => {
   return (
-    <div className="">
+    /*     <div className="">
       <div className="">
         <h2>About Agency</h2>
         <h2>
-          We create digital ideas that are bigger, bolder,braver and better.
+         
         </h2>
         <p>
           We create digital ideas that are bigger,bolder nad braver.We believe
@@ -19,6 +19,23 @@ const AboutPage = () => {
         </p>
       </div>
       <div className=""></div>
+    </div> */
+    <div className="hero min-h-screen ">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <Image src="/about.png" height="500" width="500" />
+        <div className="max-w-[50%]">
+          <h1 className="text-5xl font-bold">
+            {" "}
+            We create digital ideas that are bigger,bolder, braver and better.
+          </h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
     </div>
   );
 };
