3dd29d27b78a42c62cfdf36a58598c1340e3eaae
AlamRazek <ohidul933@gmail.com>
2024-04-06 03:52:42 +0100

    button added in form

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,11 +3,46 @@ import React from "react";
 
 const ContactPage = () => {
   return (
-    <div>
-      <div>
-        <Image src="contact.png" alt="" width={500} height={500} />
+    <div className=" flex md:flex-row flex-col items-center gap-24 md:my-24">
+      <div className="flex-1 relative ">
+        <Image
+          src="/contact.png"
+          alt=""
+          height={500}
+          width={500}
+          className="object-contain w-full h-auto"
+        />
       </div>
-      <div></div>
+      <div className="flex-1 ">
+        <form action="" className="flex flex-col gap-5 ">
+          <input
+            type="text"
+            placeholder="Name and Surname"
+            className="p-5 rounded border-none outline-none bg-[#2d2b42] text-white"
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            className="p-5 rounded border-none outline-none bg-[#2d2b42] text-white"
+          />
+          <input
+            type="text"
+            placeholder="Phone (optional)"
+            className="p-5 rounded border-none outline-none bg-[#2d2b42] text-white"
+          />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Message"
+            className="p-5 rounded border-none outline-none bg-[#2d2b42] text-white"
+          ></textarea>
+          <button className="text-white bg-[#3673fd]  rounded my-2 py-1 font-bold border-none">
+            Send
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
