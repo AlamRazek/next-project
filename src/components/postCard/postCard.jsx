7a0a20d2cadce5b3fce8dfe6a3bca490dda8c128
AlamRazek <ohidul933@gmail.com>
2024-04-07 03:03:18 +0100

    postycard image added

--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const PostCard = () => {
-  return <div>hello</div>;
+  return (
+    <div className="flex flex-col gap-5 mb-5">
+      <div className="flex">
+        <div className="w-[90%] h-[400px] relative">
+          <Image
+            src="https://images.pexels.com/photos/3912838/pexels-photo-3912838.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            fill
+            className="object-cover"
+          />
+        </div>
+        <span className="">01.01.2024</span>
+      </div>
+      <div>
+        <h1 className="">Title</h1>
+        <p className="">Desc</p>
+        <Link href="/blog/post" className="">
+          Read More
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default PostCard;
