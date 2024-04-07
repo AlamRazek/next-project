48676b433e40bbf4d29b97c09fe0b9e8c031948c
AlamRazek <ohidul933@gmail.com>
2024-04-07 03:47:06 +0100

    postcard created

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,11 +3,19 @@ import React from "react";
 
 const blogPage = () => {
   return (
-    <div className="">
-      <PostCard />
-      <PostCard />
-      <PostCard />
-      <PostCard />
+    <div className="flex flex-wrap gap-5">
+      <div className="w-full md:w-[45%] xl:w-[30%]">
+        <PostCard />
+      </div>
+      <div className="w-full md:w-[45%] xl:w-[30%]">
+        <PostCard />
+      </div>
+      <div className="w-full md:w-[45%] xl:w-[30%]">
+        <PostCard />
+      </div>
+      <div className="w-full md:w-[45%] xl:w-[30%]">
+        <PostCard />
+      </div>
     </div>
   );
 };
--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -5,7 +5,7 @@ const PostCard = () => {
   return (
     <div className="flex flex-col gap-5 mb-5">
       <div className="flex">
-        <div className="w-[90%] h-[400px] relative">
+        <div className="w-[90%] h-96  relative">
           <Image
             src="https://images.pexels.com/photos/3912838/pexels-photo-3912838.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             alt=""
@@ -13,12 +13,20 @@ const PostCard = () => {
             className="object-cover"
           />
         </div>
-        <span className="">01.01.2024</span>
+        <span className="text-sm transform rotate-[270deg] m-auto">
+          01.01.2024
+        </span>
       </div>
       <div>
-        <h1 className="">Title</h1>
-        <p className="">Desc</p>
-        <Link href="/blog/post" className="">
+        <h1 className="text-xl font-bold mb-5 w-[85%]">Title</h1>
+        <p className="mb-5 font-light text-gray-400 w-[85%]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima nemo
+          provident quasi maxime suscipit ab? Corporis praesentium, nihil
+          aliquid sint perferendis at neque nemo, reiciendis quia esse ducimus
+          perspiciatis? Ipsa culpa quia id? Excepturi nulla tempore ut vel!
+          Eius, officiis!
+        </p>
+        <Link href="/blog/post" className="underline">
           Read More
         </Link>
       </div>
