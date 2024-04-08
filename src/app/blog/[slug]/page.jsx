a474744e3c0c8c69d337e5332bb07c0822a6192e
AlamRazek <ohidul933@gmail.com>
2024-04-08 04:06:17 +0100

    title added

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,5 +1,28 @@
+import Image from "next/image";
+
 const SinglePostPage = () => {
-  return <div>this is title page</div>;
+  return (
+    <div>
+      <div className="">
+        <Image
+          src="https://images.pexels.com/photos/3912838/pexels-photo-3912838.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+          fill
+          className=""
+        />
+      </div>
+      <div className="">
+        <h1 className="">Title</h1>
+        <div className="">
+          <Image
+            src="https://images.pexels.com/photos/17894070/pexels-photo-17894070/free-photo-of-ring-tailed-lemur-sitting-on-a-fence.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            fill
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePostPage;
