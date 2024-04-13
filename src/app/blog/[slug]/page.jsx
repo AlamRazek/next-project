43d8cb7edd5c44ee27c64cc9593599845f67a3e4
AlamRazek <ohidul933@gmail.com>
2024-04-13 02:49:17 +0100

    image upadted

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -3,34 +3,34 @@ import Image from "next/image";
 const SinglePostPage = () => {
   return (
     <div className="flex gap-24">
-      <div className="flex-1 relative w-[calc(100vh-200px)]">
+      <div className="flex-1  relative h-[calc(100vh-200px)]">
         <Image
           src="https://images.pexels.com/photos/3912838/pexels-photo-3912838.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt=""
-          fill
-          className="object-cover"
+          layout="fill"
+          className="object-cover lg:block hidden"
         />
       </div>
       <div className="flex-[2] flex flex-col gap-12 ">
         <h1 className="text-6xl">Title</h1>
-        <div className="flex  gap-5">
+        <div className="flex   gap-5">
           <Image
             src="https://images.pexels.com/photos/17894070/pexels-photo-17894070/free-photo-of-ring-tailed-lemur-sitting-on-a-fence.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             alt=""
             height={50}
             width={50}
-            className="object-cover rounded-[50%] "
+            className="object-cover rounded-[50%] hidden lg:block"
           />
           <div className="flex flex-col gap-2">
-            <span className="">Author</span>
-            <span className="">Terry Jefferson</span>
+            <span className="text-gray-300 font-bold">Author</span>
+            <span className="font-medium">Terry Jefferson</span>
           </div>
           <div className="flex flex-col gap-2">
             <span className="">Published</span>
             <span className="">01.01.2024</span>
           </div>
         </div>
-        <div className="">
+        <div className="text-xl">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
           reprehenderit ratione fuga alias ab placeat nihil illo quia adipisci,
           voluptatibus laborum! Quisquam obcaecati nemo saepe consequuntur unde
